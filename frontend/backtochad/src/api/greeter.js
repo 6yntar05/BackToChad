b2df8b9634a3b6899ea97b371f9fc0c8a230ffaf
Dmitry Zhavoronkov <rudomitori@gmail.com>
2022-12-10 19:52:42 +0700

    Add login check method

--- frontend/backtochad/src/api/greeter.js
+++ frontend/backtochad/src/api/greeter.js
@@ -1,22 +1,31 @@
 import {AuthClient} from "./grpc/UsersServiceClientPb";
-import {LoginRequestDto} from "./grpc/users_pb";
+import {CheckRequestDto, LoginRequestDto} from "./grpc/users_pb";
 
 const authClient = new AuthClient("");
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJMb2dpbiI6IlRlc3RMb2dpbiIsImV4cCI6MTY3MDk4Njk1OSwiaXNzIjoiRGV2QmFja2VuZCIsImF1ZCI6IkRldkF1ZGllbmNlIn0.Tm3_EN9OyA8oUl2lS7cNIa0Nykf7cwH0zrZO6YnBsEs";
-const metadata = {'Authorization': `Bearer ${token}`};
-
-export const login = async () => {
-
+export const login = async (login, password, token) => {
     const loginRequestDto = new LoginRequestDto();
-    loginRequestDto.setLogin("TestUser");
-    loginRequestDto.setPassword("SecretPassword228+");
+    loginRequestDto.setLogin(login);
+    loginRequestDto.setPassword(password);
 
-    try {
-        const resp = await authClient.login(loginRequestDto, metadata)
-        console.log("Response", resp.toObject());
-    } catch (e) {
-        console.error(e)
-    }
+    const metadata = token ? {'Authorization': `Bearer ${token}`} : {};
+
+    const resp = await authClient.login(loginRequestDto, metadata)
+        .then(x => x.toObject());
+
+    if(resp.error)
+        throw Error(resp.error);
+
+    return resp;
+}
+
+export const checkLogin = async (token) => {
+    const checkRequestDto = new CheckRequestDto();
+    const metadata = token ? {'Authorization': `Bearer ${token}`} : {};
+
+    const resp = await authClient.check(checkRequestDto, metadata)
+        .then(x => x.toObject());
+
+    return resp;
 }
 
